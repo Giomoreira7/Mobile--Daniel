ff3c75536df522af7502200cfa777969d5505ad5
Giomoreira7 <gi.ovannamoreiradasilva2710@gmail.com>
2025-02-27 20:39:49 -0300

    g

--- Aula27.02/Exemplo06.dart
+++ Aula27.02/Exemplo06.dart
@@ -1,18 +1,18 @@
- /*Cria uma classe denominada animal com o método dormir.
- Cria uma classe denominada cachorro herdando o método dormir da
-classe animal.
- */
-
 class Animal{
-  String? nome;
-  int? idade;
+  String nome;
+  int idade;
+
+  // construtor 
+  Animal(this.nome,this.idade);
   void dormir(){
     print("O animal ${nome} esta dormindo");
   }
 }
 
 
+
 class Cachorro extends Animal{
+  Cachorro(nome,idade):super(nome,idade);
   void latir(){
     print("O animal ${nome} esta latindo");
   }
@@ -20,20 +20,23 @@ class Cachorro extends Animal{
 
 
 class Tigre extends Animal{
+  Tigre(nome,idade,this.cor):super(nome,idade);
   String? cor;
   void atacar(){
     print("O animal Tigre $nome atacou");
   }
 }
 
+
 void main(){
-  Cachorro britney = Cachorro();
+  
+  Cachorro britney = Cachorro("britney",2);
   britney.nome="britney";
   britney.idade =2;
   britney.latir();
   britney.dormir();
-
-  Tigre Garro = Tigre();
+ 
+  Tigre Garro = Tigre("Garro",30,"Branco");
   Garro.cor="Branco";
   Garro.nome = "Garro";
   Garro.idade = 30;
