ecd58cc763e9e24e302ef647d29b66456323c40b
Giomoreira7 <gi.ovannamoreiradasilva2710@gmail.com>
2025-02-27 21:36:35 -0300

    g

--- Aula27.02/Exercicios/Ex01.dart
+++ Aula27.02/Exercicios/Ex01.dart
@@ -1,5 +1,4 @@
-/* 1. Crie uma classe mãe chamada “Animal” com os atributos:
-String nome, int idade, String cor, String raça.
+/* 1. Crie uma classe mãe chamada “Animal” com os atributos: String nome, int idade, String cor, String raça.
 */
 
 class Animal{
