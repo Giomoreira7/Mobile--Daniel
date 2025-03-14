71a402b83601b32f29dc82a673f7061e90915a36
Giomoreira7 <gi.ovannamoreiradasilva2710@gmail.com>
2025-03-13 21:39:37 -0300

    g

--- Aula13.03/aulaapp1303/flutter_application_1/lib/main.dart
+++ Aula13.03/aulaapp1303/flutter_application_1/lib/main.dart
@@ -11,13 +11,29 @@ class TelaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      
+      debugShowCheckedModeBanner: false,
       home: Column(
+        
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         
         children: [
+          Container(width: 200,color: Colors.blueGrey,),
+          Stack(
+            alignment:Alignment.center,
+            children: [
+              Container(width: 200,height: 100,color: Colors.deepOrange,),
+              Container(width: 150,height: 80,color: Colors.teal,
+              child: Text("SENAI",textAlign:TextAlign.center,style: TextStyle(color: Colors.white,
+              decoration: TextDecoration.none),))
+            ],
+          ),
           Container(width: 200,height: 200,color: Colors.pink,),
-          Text("Aula 13.03"),
-          Container(width: 200,height: 200,color: Colors.blue,),
+          Text("Aula 13.03",style:TextStyle(fontSize: 30,decoration:TextDecoration.none, color: Colors.white),),
+          Container(width: 200,height: 100,color: Colors.red,),
+          ElevatedButton(onPressed: (){
+            print("Botao pressionado");
+          }, child: Text("Mensagem")),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
