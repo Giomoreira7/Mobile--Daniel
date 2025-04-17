2dad759a549c8242b67fd442436a87b6b24d478e
Giovanna Moreira <gi.ovannamoreiradasilva2710@gmail.com>
2025-04-17 19:15:36 -0300

    Update main.dart

--- Aula20.03/appex1/lib/main.dart
+++ Aula20.03/appex1/lib/main.dart
@@ -1,23 +1,23 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(TelaApp());
+  runApp(const ContTech());
 }
 
-class TelaApp extends StatelessWidget {
-  const TelaApp({super.key});
+class ContTech extends StatelessWidget {
+  const ContTech({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Atividade 01"),
-        ),
-        body: Home(),
-      ),
-    );
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("Atividade-08"),
+          ),
+
+          body: Home(),
+        ));
   }
 }
 
@@ -29,154 +29,87 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  // Variavel mensagem
-  String msg = "Aula Daniel";
+  int contador = 0;
+  String nome = "";
 
-  // Função para exibir mensagem no app
-  _exibemsg() {
+
+  _incrementar() {
     setState(() {
-      msg = "Mobile";
+      contador++;
     });
   }
 
-  // Função para limpar mensagem
-  _limpar() {
+
+  _resetar() {
     setState(() {
-      msg = "";
+      contador = 0;
     });
   }
 
-  @override
+  _exibeNome() {
+    setState(() {
+      nome = "Giovanna";
+    });
+  }
+
+  _limparNome() {
+    setState(() {
+      nome = "";
+    });
+  }
+
   Widget build(BuildContext context) {
     return Column(
       children: [
-        // SizedBox for spacing
         SizedBox(
           height: 20,
         ),
-        
-        // Stack to display colored containers with text in the center
         Stack(
           alignment: Alignment.center,
           children: [
             Container(
-              width: 2000,
-              height: 200,
-              color: const Color.fromARGB(255, 6, 155, 192),
-            ),
-            Container(
-              alignment: Alignment.center,
-              width: 1000,
+              width: 200,
               height: 100,
-              color:const Color.fromARGB(255, 121, 182, 197),
+              alignment: Alignment.center,
+              color: const Color.fromARGB(255, 176, 91, 255),
               child: Text(
-                "$msg",
+                "Contador: ${contador}",
                 style: TextStyle(fontSize: 30, color: Colors.white),
                 textAlign: TextAlign.center,
               ),
             ),
           ],
         ),
-
-        // SizedBox for spacing
         SizedBox(
-          height: 20,
+          height: 250,
         ),
-
-        // ListView with ListTiles
-        
-
-        // Row with buttons and icons
+        Stack(
+          alignment: Alignment.center,
+          children: [
+            Container(
+              width: 200,
+              height: 100,
+              alignment: Alignment.center,
+              color: const Color.fromARGB(255, 174, 152, 255),
+              child: Text(
+                nome,
+                style: TextStyle(fontSize: 30, color: Colors.white),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ],
+        ),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            TextButton(
-              onPressed: _exibemsg,
-              child: Text("Mensagem", style: TextStyle(fontSize: 20,color: const Color.fromARGB(255, 2, 123, 142)
-
-              ),),
-            ),
-            TextButton(
-              onPressed: _limpar,
-              child: Text("Limpar", style: TextStyle(
-                fontSize: 20,color: const Color.fromARGB(255, 2, 123, 142)
-              ),),
-            ),
-
-        
+            IconButton(onPressed: _incrementar, icon: Icon(Icons.plus_one)),
+            IconButton(onPressed: _resetar, icon: Icon(Icons.restart_alt)),
+            IconButton(onPressed: _exibeNome, icon: Icon(Icons.visibility)),
+            IconButton(
+                onPressed: _limparNome, icon: Icon(Icons.visibility_off)),
           ],
         ),
       ],
-    );
-  }
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
